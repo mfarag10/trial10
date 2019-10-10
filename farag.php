3b6ea44f89dc4883dcbcacbdf62b6deb27143263
mfarag10 <44197060+mfarag10@users.noreply.github.com>
2019-10-10 08:09:46 +0200

    Update farag.php

--- farag.php
+++ farag.php
@@ -1,3 +1,3 @@
 <?php
-echo "hello openshift...Awesome openshift -----hi to meko meko meko & beba beba beba world 22-august meeting";
+echo "hello openshift...Awesome openshift -----hi to meko meko meko & beba beba beba world 10-october meeting";
 ?>
