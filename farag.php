90787228ebb9c554b4c84693dd1f3e04447cbc10
mfarag10 <44197060+mfarag10@users.noreply.github.com>
2020-04-13 01:17:41 +0200

    Update farag.php

--- farag.php
+++ farag.php
@@ -1,3 +1,3 @@
 <?php
-echo "Bank Misr 123124124123hello openshift...Awesome openshift -----Hello and hi and hell to meko meko meko & beba beba beba world 10-october meeting";
+echo "Bank Misr--amr salah 123124124123hello openshift...Awesome openshift -----Hello and hi and hell to meko meko meko & beba beba beba world 10-october meeting";
 ?>
